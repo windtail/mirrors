7047b6bbf10e8a4d1c1e13d0d8c91901172fd569
luojiejun <6020100326ljj@163.com>
2018-06-19 19:15:52 +0800

    apt mirror may change repoes

--- apt-mirror/setup.sh
+++ apt-mirror/setup.sh
@@ -6,7 +6,7 @@ cat > /etc/apt/mirror.list <<EOF
 set base_path /apt-mirror
 set nthreads  20
 set _tilde 0
-deb-$ARCH $UPSTREAM $RELEASE main contrib non-free main/debian-installer contrib/debian-installer non-free/debian-installer
+deb-$ARCH $UPSTREAM $RELEASE $REPOES
 EOF
 
 exec "$@"
